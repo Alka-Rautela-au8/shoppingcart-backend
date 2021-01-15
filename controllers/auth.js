f5175a48a4a7dfcc9c5c2ea5b7567908dbced2f9
alka-rautela-au8 <alka.rautela@icloud.com>
2021-01-15 07:09:50 +0530

    bug fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -364,15 +364,15 @@ const sendTokenResponse = (user, statusCode, res) => {
 
     const options = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true,
+        httpOnly: false,
         sameSite: 'none',
         secure: false
     }
 
-    // secure flag for production mode
-    if(process.env.NODE_ENV === 'production'){
-        options.secure = true;
-    }
+    // // secure flag for production mode
+    // if(process.env.NODE_ENV === 'production'){
+    //     options.secure = true;
+    // }
 
     res
         .status(statusCode)
